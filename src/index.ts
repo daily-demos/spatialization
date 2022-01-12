793724be657f7c0cbd2943822feba6ef4c55f056
lizadaily <93514641+lizadaily@users.noreply.github.com>
2022-01-12 10:41:58 +0100

    Update src/index.ts
    Co-authored-by: Mattie Ruth <mattieruth@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,10 @@ let globalRoom;
 
 export function initCall(name: string, url: string) {
   // We will do this in rooms, in case we want to implement
-  // breakout rooms later. Each room will have its own call
-  // object. There is one "global" room.
+  // breakout rooms later. Each room will have its own instance of
+  // the daily call object. There is one "global" room. Note that daily
+  // only supports one call object instance at a time so there will only
+  // ever be one "global" room.
   globalRoom = new Room(url, name, true);
   globalRoom.join();
 }
