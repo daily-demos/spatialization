25c7aafd985116ab5e5a7cdb31c6c953a61820e1
Liza <liza@daily.co>
2022-01-24 15:54:27 +0100

    remove unneeded var I noticed while writing post 2

--- src/index.ts
+++ src/index.ts
@@ -14,14 +14,11 @@ import "./assets/microphone.svg";
 
 registerJoinFormListener(initCall);
 
-let globalRoom;
-
 export function initCall(name: string, url: string) {
   // We will do this in rooms, in case we want to implement
   // breakout rooms later. Each room will have its own instance of
-  // the daily call object. There is one "global" room. Note that daily
-  // only supports one call object instance at a time so there will only
-  // ever be one "global" room.
-  globalRoom = new Room(url, name, true);
+  // the daily call object. There is one "global" room. Note that Daily
+  // only supports one call object instance at a time.
+  const globalRoom = new Room(url, name, true);
   globalRoom.join();
 }
