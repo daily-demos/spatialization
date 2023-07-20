4b4e206b4a14565666db442e6a35b61cc90c75fc
Liza Shulyayeva <65890040+lazeratops@users.noreply.github.com>
2023-07-20 16:50:12 +0200

    readd space

--- jest.config.ts
+++ jest.config.ts
@@ -183,6 +183,7 @@ export default {
   transform: {
     "^.+\\.ts?$": "ts-jest",
   },
+
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
   //   "\\\\node_modules\\\\",
