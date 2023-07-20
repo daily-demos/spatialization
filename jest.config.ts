d9b2098a1c437ea335e00422fe779416e915cd33
Liza Shulyayeva <65890040+lazeratops@users.noreply.github.com>
2023-07-20 16:49:23 +0200

    comment out transformIgnorePatterns

--- jest.config.ts
+++ jest.config.ts
@@ -184,10 +184,10 @@ export default {
     "^.+\\.ts?$": "ts-jest",
   },
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  transformIgnorePatterns: [
+  // transformIgnorePatterns: [
   //   "\\\\node_modules\\\\",
   //   "\\.pnp\\.[^\\\\]+$"
-  ],
+  // ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
